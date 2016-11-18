6bce117563861105217b9e4284a33974b7b3a58c
Dirk de Boer <stofkat@gmail.com>
2016-11-18 14:12:50 +0100

    new
    lala

---
+++ src/main/java/com/stofkat/toffeestun/UDPPeer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stofkat.toffeestun;
+
+import java.net.DatagramPacket;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ *
+ * @author Dirk
+ */
+public class UDPPeer {
+
+    
+    public String ipAddress;
+    public String guid;
+    public int port;
+    public String clientKey;
+    public String content;
+    public boolean isServer;
+
+
+    public UDPPeer(DatagramPacket packet) {
+
+        ipAddress = packet.getAddress().toString().substring(1);
+        port = packet.getPort();
+        content = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(content + ":" + ipAddress + ":" + port);
+        
+    }
+
+}
